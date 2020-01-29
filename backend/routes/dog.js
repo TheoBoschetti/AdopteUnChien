f720742c03d1d8c273f939e60400f669c865ea6f
Theo Boschetti <boschetti.theo@gmail.com>
2020-01-29 23:21:50 +0100

    update dog routes

--- backend/routes/dog.js
+++ backend/routes/dog.js
@@ -4,14 +4,15 @@ const router = express.Router();
 
 //Get dogs
 router.get("/", (req, res) => {
-  connection.query(
-    "SELECT * FROM dog WHERE adoption_date IS NULL",
-    (err, result) => {
-      if (err)
-        return res.status(500).send("Error has occured during the operation.");
-      return res.status(200).json(result);
-    }
-  );
+  let sql = "SELECT * FROM dog WHERE adoption_date IS NULL";
+  if (req.query.home) {
+    sql += " ORDER BY id DESC LIMIT 3";
+  }
+  connection.query(sql, (err, result) => {
+    if (err)
+      return res.status(500).send("Error has occured during the operation.");
+    return res.status(200).json(result);
+  });
 });
 
 module.exports = router;
